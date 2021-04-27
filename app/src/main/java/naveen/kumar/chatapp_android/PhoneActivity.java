0080e552e1231780455578816ce3939957aa46a0
naveenkumar <naveenbti002@gmail.com>
2021-04-27 17:27:02 -0400

    init commit

---
+++ app/src/main/java/naveen/kumar/chatapp_android/PhoneActivity.java
@@ -0,0 +1,14 @@
+package naveen.kumar.chatapp_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PhoneActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_phone);
+    }
+}
